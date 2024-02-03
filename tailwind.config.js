efdace80b4f8601acdd0dc223f0332ae262b6f46
KreativeThinker <sagely2005eagle@gmail.com>
2024-02-03 20:15:50 +0530

    Added About me and more projects

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,astro}"],
+  theme: {
+    fontFamily: {
+      sans: ['"Montserrat"'],
+    },
+    colors: {
+      midnight: "#121212",
+      black: "#202232",
+      navy: "#2B2D42",
+      white: "#EDF2F4",
+      crimson: "#EF233C",
+      red: "#F0374E",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
